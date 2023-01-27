7e64c0863d26906f54dbe3dc80fa8ac1cae5f94e
Lucas Marin <96667789+marinSantos@users.noreply.github.com>
2023-01-27 16:18:26 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Mensagem de erro
+let erroB = document.getElementById("erroB");
+
+function erro() {
+  get.classList.add("msg");
+}
+
+erroB.addEventListener("click", erro);
+erroB.addEventListener("toachstart", erro);
